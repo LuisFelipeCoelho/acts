85751268ed6fdbc9bb1e8c8be34d5a3bb3cc14c4
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2023-03-10 17:53:03 +0100

    itk

--- Examples/Scripts/Python/full_chain_itk.py
+++ Examples/Scripts/Python/full_chain_itk.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import pathlib, acts, acts.examples, acts.examples.itk
-from acts.examples import CsvSpacePointReader
+from acts.examples import CsvSpacePointReader, RootParticleReader
 from acts.examples.simulation import (
     addParticleGun,
     MomentumConfig,
@@ -36,20 +36,41 @@ geo_dir = pathlib.Path("/Users/luiscoelho/lcoelho/acts/acts-itk")
 outputDir = pathlib.Path.cwd() / "itk_output"
 # acts.examples.dump_args_calls(locals())  # show acts.examples python binding calls
 
-# detector, trackingGeometry, decorators = acts.examples.itk.buildITkGeometry(geo_dir)
+detector, trackingGeometry, decorators = acts.examples.itk.buildITkGeometry(geo_dir)
 field = acts.examples.MagneticFieldMapXyz(str(geo_dir / "bfield/ATLAS-BField-xyz.root"))
 rnd = acts.examples.RandomNumbers(seed=42)
 
-s = acts.examples.Sequencer(events=1, numThreads=1, outputDir=str(outputDir))
+s = acts.examples.Sequencer(events=20, numThreads=4, outputDir=str(outputDir))
 
-# s.addReader(
-# 		RootParticleReader(
-# 				level=acts.logging.INFO,
-# 				filePath="/Users/luiscoelho/lcoelho/acts2/samples/ttbar_mu200_500evts/pythia8_particles.root",
-# 				particleCollection="particles_input",
-# 				orderedEvents=False,
-# 		)
-# )
+s.addReader(
+	RootParticleReader(
+			level=acts.logging.INFO,
+			filePath="/Users/luiscoelho/lcoelho/acts2/samples/ttbar_mu200_500evts/pythia8_particles.root",
+			particleCollection="particles_input",
+			orderedEvents=False,
+	)
+)
+ 
+addFatras(
+    s,
+    trackingGeometry,
+    field,
+    rnd=rnd,
+    preSelectParticles=ParticleSelectorConfig(
+        eta=(-4.0, 4.0), pt=(150 * u.MeV, None), removeNeutral=True
+    )
+    if ttbar_pu200
+    else ParticleSelectorConfig(),
+    outputDirRoot=outputDir,
+)
+
+addDigitization(
+    s,
+    trackingGeometry,
+    field,
+    digiConfigFile=geo_dir / "itk-hgtd/itk-smearing-config.json",
+    outputDirRoot=outputDir,
+)
 
 pixel = True
 
@@ -62,36 +83,36 @@ else:
     inputCollection = "strip"
     extendCollection = True
 
-# Read input space points from input csv files
-evReader = CsvSpacePointReader(
-    level=acts.logging.INFO,
-    inputStem="spacepoints",
-    inputCollection=inputCollection,
-    inputDir="/Users/luiscoelho/lcoelho/acts/data/CsvSpacePointsOutput_ttbar",
-    outputSpacePoints=inputSpacePointsType,
-    extendCollection=extendCollection,
-)
+## Read input space points from input csv files
+#evReader = CsvSpacePointReader(
+#    level=acts.logging.INFO,
+#    inputStem="spacepoints",
+#    inputCollection=inputCollection,
+#    inputDir="/Users/luiscoelho/lcoelho/acts/data/CsvSpacePointsOutput_ttbar",
+#    outputSpacePoints=inputSpacePointsType,
+#    extendCollection=extendCollection,
+#)
+#
+#s.addReader(evReader)
 
-s.addReader(evReader)
-
-# addSeeding(
-#    s,
-#    trackingGeometry,
-#    field,
-#    TruthSeedRanges(pt=(1.0 * u.GeV, None), eta=(-4.0, 4.0), nHits=(9, None))
-#    if ttbar_pu200
-#    else TruthSeedRanges(),
-#    seedingAlgorithm=SeedingAlgorithm.Default,
-#    *acts.examples.itk.itkSeedingAlgConfig(inputSpacePointsType),
-#    geoSelectionConfigFile=geo_dir / "itk-hgtd/geoSelection-ITk.json",
-#    outputDirRoot=outputDir,
-# )
+addSeeding(
+    s,
+    trackingGeometry,
+    field,
+    TruthSeedRanges(pt=(1.0 * u.GeV, None), eta=(-4.0, 4.0), nHits=(9, None))
+    if ttbar_pu200
+    else TruthSeedRanges(),
+    seedingAlgorithm=SeedingAlgorithm.Default,
+    *acts.examples.itk.itkSeedingAlgConfig(inputSpacePointsType),
+    geoSelectionConfigFile=geo_dir / "itk-hgtd/geoSelection-ITk.json",
+    outputDirRoot=outputDir,
+ )
 
 # run seeding
-addStandardSeeding(
-    s,
-    evReader.config.outputSpacePoints,
-    *acts.examples.itk.itkSeedingAlgConfig(inputSpacePointsType),
-)
+#addStandardSeeding(
+#    s,
+#    evReader.config.outputSpacePoints,
+#    *acts.examples.itk.itkSeedingAlgConfig(inputSpacePointsType),
+#)
 
 s.run()
