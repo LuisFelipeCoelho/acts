295c7191fa0d8252cb5802457e201d596aa1d74c
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2023-03-21 09:41:44 +0100

    remove middle SPs in last layer
    Conflicts:
    	Core/include/Acts/Seeding/SeedFinder.ipp

--- Examples/Python/python/acts/examples/itk.py
+++ Examples/Python/python/acts/examples/itk.py
@@ -304,6 +304,8 @@ def itkSeedingAlgConfig(inputSpacePointsType: InputSpacePointsType):
     # variables that do not change for pixel and strip SPs:
     zMax = 3000 * u.mm
     zMin = -3000 * u.mm
+    zMinLastLayer =  2700 * u.mm
+    zMaxLastLayer =  -2700 * u.mm
     beamPos = (0 * u.mm, 0 * u.mm)
     collisionRegionMin = -200 * u.mm
     collisionRegionMax = 200 * u.mm
@@ -521,6 +523,7 @@ def itkSeedingAlgConfig(inputSpacePointsType: InputSpacePointsType):
         collisionRegion=(collisionRegionMin, collisionRegionMax),
         r=(None, rMaxSeedFinderConfig),
         z=(zMin, zMax),
+        zLastLayer=(zMinLastLayer, zMaxLastLayer),
     )
 
     seedFinderOptionsArg = SeedFinderOptionsArg(bFieldInZ=bFieldInZ, beamPos=beamPos)
--- Examples/Python/src/TrackFinding.cpp
+++ Examples/Python/src/TrackFinding.cpp
@@ -85,6 +85,8 @@ void addTrackFinding(Context& ctx) {
     ACTS_PYTHON_MEMBER(phiMax);
     ACTS_PYTHON_MEMBER(zMin);
     ACTS_PYTHON_MEMBER(zMax);
+    ACTS_PYTHON_MEMBER(zMinLastLayer);
+    ACTS_PYTHON_MEMBER(zMaxLastLayer);
     ACTS_PYTHON_MEMBER(rMax);
     ACTS_PYTHON_MEMBER(rMin);
     ACTS_PYTHON_MEMBER(radLengthPerSeed);
@@ -147,6 +149,8 @@ void addTrackFinding(Context& ctx) {
     ACTS_PYTHON_MEMBER(phiMax);
     ACTS_PYTHON_MEMBER(zMin);
     ACTS_PYTHON_MEMBER(zMax);
+    ACTS_PYTHON_MEMBER(zMinLastLayer);
+    ACTS_PYTHON_MEMBER(zMaxLastLayer);
     ACTS_PYTHON_MEMBER(rMax);
     ACTS_PYTHON_MEMBER(rMin);
     ACTS_PYTHON_MEMBER(radLengthPerSeed);
