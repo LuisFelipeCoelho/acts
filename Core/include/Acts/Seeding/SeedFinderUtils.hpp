e099b5ba9a1f6609d33ccb420ee2876109406cb8
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2023-03-30 12:58:45 +0200

    back to double

--- Core/include/Acts/Seeding/SeedFinderUtils.hpp
+++ Core/include/Acts/Seeding/SeedFinderUtils.hpp
@@ -94,7 +94,7 @@ bool xyzCoordinateCheck(
     Acts::SpacePointData& spacePointData,
     const Acts::SeedFinderConfig<external_spacepoint_t>& config,
     const Acts::InternalSpacePoint<external_spacepoint_t>& sp,
-    const float* spacepointPosition, float* outputCoordinates);
+    const double* spacepointPosition, double* outputCoordinates);
 
 }  // namespace Acts
 
